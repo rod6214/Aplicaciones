67d1ac3a4e08e4927f2b9e1f5fcb22661aacba83
Amador <BABC10A@ccba.usr.bpba>
2018-08-13 11:04:23 -0300

    .

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -6,10 +6,11 @@ var loginController =  function(){}
 loginController.prototype = new mvc.controllerBase;
 
 loginController.prototype.init = function(){
-    this.get('/views/login', 'login/index.ejs', GetAll);
+    this.get('/views/login', 'login/index.ejs', GetPage);
+    this.post('/views/login', '');
 }
 
-function GetAll(view){
+function GetPage(view){
     // console.log(clearCookie);
     console.log(view.res.cookie);
     // console.log(view.__proto__);
